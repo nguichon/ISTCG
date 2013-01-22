f47dfdd7b2f5e05c06d7b5c251860b7f9960f9fd
Jesse Harrison <jharrison902@gmail.com>
2013-01-21 20:09:17 -0500

    Added rsa encryption on messages

--- ISTCG/src/Shared/ConnectionDevice.java
+++ ISTCG/src/Shared/ConnectionDevice.java
@@ -5,10 +5,14 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
+import org.keyczar.Crypter;
+import org.keyczar.exceptions.KeyczarException;
+
 public class ConnectionDevice {
 	private Socket connectionSocket;
 	private PrintWriter output;
 	private Scanner input;
+	private Crypter crypter=null;
 
 	public ConnectionDevice( Socket socket ) throws IOException {
 		connectionSocket = socket;
@@ -16,23 +20,52 @@ public class ConnectionDevice {
 		input = new Scanner(connectionSocket.getInputStream());
 		output = new PrintWriter(connectionSocket.getOutputStream());
 		
+		try {
+			crypter = new Crypter(".");
+		} catch (KeyczarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 	public ConnectionDevice( String host, int port ) throws IOException {
 		connectionSocket = new Socket( host, port );
 		
 		input = new Scanner(connectionSocket.getInputStream());
 		output = new PrintWriter(connectionSocket.getOutputStream());
+		
+		try {
+			crypter = new Crypter(".");
+		} catch (KeyczarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public String getData() {
 		if( input.hasNext() ) {
-			return input.next();
+			String s = "";
+			try {
+				s = crypter.decrypt(input.next());
+			} catch (KeyczarException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return s;
 		}
 		return "";
 	}
 	
 	public void sendData( String Data ) {
-		output.println( Data );
+		String s = "";
+		try {
+			s = crypter.encrypt(Data);
+		} catch (KeyczarException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		output.println( s );
 		output.flush();
 	}
 	
