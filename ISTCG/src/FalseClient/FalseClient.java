d5b61cc15215433af716e818c3caca40f62992bd
grogian <nicholas.guichon.10@cnu.edu>
2013-01-23 18:41:14 -0500

    False client was not uploaded. My bad, brah.

---
+++ ISTCG/src/FalseClient/FalseClient.java
@@ -0,0 +1,46 @@
+package FalseClient;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import Client.LoginUI;
+import Client.ClientMain.GameState;
+import Shared.ConnectionDevice;
+
+public class FalseClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner consoleInput = new Scanner(System.in);
+		
+		int port = 4567;
+  		String host = "127.0.0.1";
+		ConnectionDevice m_Server = null;
+		try {
+			m_Server = new ConnectionDevice( host, port );
+		} catch (IOException e) {
+			System.exit(2000);
+		}
+		
+		boolean m_Quit = false;
+		while( !m_Quit ) {
+			if(consoleInput.hasNext()) {
+				String s = consoleInput.nextLine();
+				if( s.equals( "quit" ) ) {
+					m_Quit = true;
+				} else  {
+					m_Server.sendData( s );
+				}
+				String serverData;
+				if( ( serverData = m_Server.getData() ) != "" ) {
+					System.out.println( serverData );
+				}
+			}
+		}
+		
+		
+	}
+
+}
---
+++ ISTCG/src/Server/LobbyManager.java
@@ -0,0 +1,9 @@
+package Server;
+
+public class LobbyManager {
+
+	public static void say(String userName, String message) {
+		
+	}
+	
+}
