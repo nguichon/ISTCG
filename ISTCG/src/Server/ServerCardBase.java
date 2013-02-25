8c072e495b1299e02510a069ee353622f10e0c2d
grogian <nicholas.guichon.10@cnu.edu>
2013-02-24 21:12:49 -0500

    -Load CardBase from a file, server side.

--- ISTCG/src/Server/ServerCardBase.java
+++ ISTCG/src/Server/ServerCardBase.java
@@ -1,10 +1,52 @@
 package Server;
 
-public class ServerCardBase extends Shared.CardBase {
+import java.io.File;
+import java.util.ArrayList;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import Shared.CardBase;
+import Shared.StatBlock;
+
+@XmlRootElement(namespace = "de.vogella.xml.jaxb.model")
+public class ServerCardBase extends Shared.CardBase {
+	private CardType m_CardType;
+	
 	@Override
-	public void LoadCard(String path) {
-		// TODO Auto-generated method stub
-		
+	public CardBase LoadCard(String path) {
+		try {
+			final Unmarshaller reader = JAXBContext.newInstance(ServerCardBase.class).createUnmarshaller();
+			
+			return (ServerCardBase) reader.unmarshal(new File(path));
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
+
+	@XmlElement(name="cardType")
+	public CardType getCardType( ) {
+		return m_CardType;
+	}
+	public void setCardType( CardType type ) {
+		m_CardType = type;
+	}
+
+	private ArrayList<StatBlock> m_Stats;
+	
+	@XmlElementWrapper(name = "statList")
+	@XmlElement(name="stat")
+	public void setStats(ArrayList<StatBlock> stats) {
+	    this.m_Stats = stats;
+	  }
+
+	  public ArrayList<StatBlock> getStats() {
+	    return m_Stats;
+	  }
 }
--- ISTCG/src/Shared/CardBase.java
+++ ISTCG/src/Shared/CardBase.java
@@ -7,23 +7,10 @@ public abstract class CardBase implements SharedObject {
 
 	private int m_CardID;
 	private CardType m_CardType;
-
-	private int m_Attack, m_Structure, m_Defense, m_GearPoints, m_Damage,
-			m_Time, m_Metal, m_Energy, m_Tech;
 	
 	//private Vector<Ability> abilities go here
 	
-	public abstract void LoadCard( String path );
-	
-	public int GetAttack() { return m_Attack; };
-	public int GetStructure() { return m_Structure; };
-	public int GetDefense() { return m_Defense; };
-	public int GetGearPoints() { return m_GearPoints; };
-	public int GetDamage() { return m_Damage; };
-	public int GetTime() { return m_Time; };
-	public int GetMetal() { return m_Metal; };
-	public int GetEnergy() { return m_Energy; };
-	public int GetTech() { return m_Tech; }
+	public abstract CardBase LoadCard( String path );
 
 	public int getID() { return m_CardID; };
 }
