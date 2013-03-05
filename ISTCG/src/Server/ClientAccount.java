c37d42520f87876c6ab5166b4854acd29722c5f5
Grogian <nicholas@guichonchi.com>
2013-03-04 20:51:38 -0500

    Temporarily, EVERYONE AN ADMIN

--- ISTCG/src/Server/ClientAccount.java
+++ ISTCG/src/Server/ClientAccount.java
@@ -206,6 +206,8 @@ public class ClientAccount extends Thread {
 						"UPDATE Users SET last_login = NOW() WHERE Users.id = "
 								+ m_UserID + ";");
 				// m_AdminAccount = rs.getBoolean("is_admin");
+				// TODO fix the check for admin, so not everyone is an admin :P
+				m_AdminAccount = true;
 			} else {
 				m_UserID = -1;
 				return false;
