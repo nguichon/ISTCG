c38165a4197009475077fcd5c2c5e7633d50e117
Grogian <nicholas@guichonchi.com>
2013-03-17 18:19:43 -0400

    Suck it git.

--- ISTCG/src/Shared/GameZones.java
+++ ISTCG/src/Shared/GameZones.java
@@ -1,3 +1,11 @@
 package Shared;
 
-public enum GameZones {	HAND, DECK, GRAVEYARD, FIELD, DELAYED, PLAYED; }
+public enum GameZones { UNKNOWN( true ), HAND( true ), DECK( true ), GRAVEYARD( false ), FIELD( false ), DELAYED( false ), STACK( false ); 
+
+	private boolean m_IsHiddenZone;
+	
+	private GameZones( boolean hiddenZone ) { m_IsHiddenZone = hiddenZone; }
+	public boolean isHiddenZone() { return m_IsHiddenZone; }
+}
+
+//WHY WONT YOU COMMIT
